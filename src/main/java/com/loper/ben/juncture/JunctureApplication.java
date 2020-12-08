a9b0a80e6ac94b9e0186cadc006436a9388bb994
Ben Loper <loperb06@gmail.com>
2020-12-07 22:28:35 -0500

    Initial commit

---
+++ src/main/java/com/loper/ben/juncture/JunctureApplication.java
@@ -0,0 +1,13 @@
+package com.loper.ben.juncture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loper/ben/juncture/JunctureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loper.ben.juncture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunctureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
